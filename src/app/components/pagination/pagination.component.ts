5bf77e4194fd35cafb991eb110598118cf2150e0
nati-grossman <natn0542489516@gmail.com>
2025-05-11 01:38:49 +0300

    fix: sticky header color and style matches regular table header

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -8,11 +8,27 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class PaginationComponent {
   @Input() currentPage: number = 1;
   @Input() totalPages: number = 1;
+  @Input() pageSize: number = 10;
+  @Input() pageSizeOptions: number[] = [5, 10, 15, 20, 25, 50];
   @Output() pageChange = new EventEmitter<number>();
+  @Output() pageSizeChange = new EventEmitter<number>();
 
   onPageChange(page: number): void {
     if (page >= 1 && page <= this.totalPages) {
       this.pageChange.emit(page);
     }
+  }
+
+  onFirstPage(): void {
+    this.onPageChange(1);
+  }
+
+  onLastPage(): void {
+    this.onPageChange(this.totalPages);
+  }
+
+  onPageSizeChange(event: Event): void {
+    const value = +(event.target as HTMLSelectElement).value;
+    this.pageSizeChange.emit(value);
   }
 }
