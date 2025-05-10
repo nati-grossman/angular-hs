5e1d89a40250caa3ce7e3aad1746c910858ff6fd
nati-grossman <natn0542489516@gmail.com>
2025-05-10 23:17:20 +0300

    fix(form): add required and disabled defaults to all fields to prevent ExpressionChangedAfterItHasBeenCheckedError

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,8 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { FieldConfig } from '../../models/field-config.model';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
-export class FormComponent {}
+export class FormComponent implements OnInit {
+  form!: FormGroup;
+
+  showFields: FieldConfig[] = [
+    {
+      type: 'text',
+      label: 'שדה טקסט רגיל',
+      name: 'textField',
+      placeholder: 'הכנס טקסט כאן',
+      required: true,
+      disabled: false,
+    },
+    {
+      type: 'text',
+      label: 'שדה טקסט מושבת',
+      name: 'disabledTextField',
+      placeholder: 'שדה מושבת',
+      required: false,
+      disabled: true,
+    },
+    {
+      type: 'radio',
+      label: 'שדה רדיו',
+      name: 'radioField',
+      options: [
+        { label: 'אפשרות 1', value: '1' },
+        { label: 'אפשרות 2', value: '2' },
+        { label: 'אפשרות 3', value: '3' },
+      ],
+      required: false,
+      disabled: false,
+    },
+    {
+      type: 'select',
+      label: 'שדה בחירה',
+      name: 'selectField',
+      options: [
+        { label: 'בחר אפשרות', value: '' },
+        { label: 'אפשרות 1', value: '1' },
+        { label: 'אפשרות 2', value: '2' },
+        { label: 'אפשרות 3', value: '3' },
+      ],
+      required: false,
+      disabled: false,
+    },
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({});
+    this.showFields.forEach((field) => {
+      this.form.addControl(field.name, this.fb.control(''));
+    });
+    console.log('showFields:', this.showFields);
+  }
+}
